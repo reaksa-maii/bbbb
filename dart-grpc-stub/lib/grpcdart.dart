d06fbeae0d8177ba4933d6e02ecc6604f4c50ba3
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-17 17:12:40 +0700

    implement package one.digital

--- dart-grpc-stub/lib/grpcdart.dart
+++ dart-grpc-stub/lib/grpcdart.dart
@@ -1,3 +1,19 @@
 int calculate() {
   return 6 * 7;
 }
+class OneClient {
+  final String host;
+  final int port;
+
+  OneClient(this.host, this.port);
+
+  void connect() {
+    // Logic to connect to the gRPC server
+    print('Connecting to gRPC server at $host:$port');
+  }
+
+  void disconnect() {
+    // Logic to disconnect from the gRPC server
+    print('Disconnecting from gRPC server at $host:$port');
+  }
+}
