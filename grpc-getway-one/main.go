4db04ded5566b279922fdf3c271319ff352b9a00
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-21 13:52:56 +0700

    change name convent package name

--- grpc-getway-one/main.go
+++ grpc-getway-one/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "../grpc-getway-client/github.com/reaksa-maii/one_digital_grpc_getway"
+import "github.com/reaksa-maii/one_digital_grpc_getway"
 
 func main() {
 	onegrpcgetway.LogInfo("master")
